54b14ccede660e2b902341b1362f1128cb101447
Kartikeya Duvvuri <142835117+kduvvuri1@users.noreply.github.com>
2025-11-30 21:11:47 -0500

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/home.svg',
+    },
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },
+    {
+        label: 'Previous',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg',
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/Video.svg',
+    },
+    {
+        label: 'AI Notes',
+        route: '/notes',
+        imgUrl: '/icons/notes.png',
+    },
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg',
+    },
+]
